7521d26dce0ddecada5c45dc0c141b2a57f82c14
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-02-06 19:16:17 +0900

    adding basic example

---
+++ Drools70Ex001_basic_kjar_test/src/test/java/com/sample/DroolsTest.java
@@ -0,0 +1,27 @@
+package com.sample;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+
+    @Test
+    public void testContainer() {
+        try {
+            KieServices ks = KieServices.Factory.get();
+            ReleaseId releaseId = ks.newReleaseId("com.sample", "Drools70Ex001_basic_kjar", "1.0.0-SNAPSHOT");
+            KieContainer kContainer = ks.newKieContainer(releaseId);
+            KieSession kSession = kContainer.newKieSession();
+
+            Person john = new Person("john", 20);
+            kSession.insert(john);
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
---
+++ Drools70Ex001_basic_person/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,29 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession();
+
+            // go !
+            Person john = new Person("john", 20);
+            kSession.insert(john);
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+
+}
