8ee0a1dd98910132dbf1283e4cea203a448cf0f4
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-04-26 16:30:50 +0900

    loop experiment

---
+++ Drools70Ex007_infinite_loop_simple/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,47 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            // go !
+            Message message = new Message();
+            kSession.insert(message);
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static class Message {
+
+        private int count = 0;
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "Message [count=" + count + "]";
+        }
+
+    }
+
+}
