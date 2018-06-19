06b8eb150d71f101a29843fe3f95b2360bf0be83
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-06-19 09:17:08 +0900

    agenda group

---
+++ Drools70Ex026_agendaGroup/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,35 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            KieSession kSession = kContainer.newKieSession();
+
+            // go !
+            Person john = new Person("john", 30);
+            kSession.insert(john);
+            kSession.fireAllRules();
+
+            kSession.getAgenda().getAgendaGroup("group1").setFocus();
+            kSession.fireAllRules();
+
+            kSession.getAgenda().getAgendaGroup("group2").setFocus();
+            kSession.fireAllRules();
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
