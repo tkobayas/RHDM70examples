ef44b5fc28b472fa776df8a0a4fe719602063bc4
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2018-11-29 16:16:24 +0900

    kie-server example

---
+++ Drools70Ex024_kie-server-client/src/main/java/com/sample/Constants.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+
+
+
+public class Constants {
+    
+    public static final String BASE_URL = "http://localhost:8080/kie-server/services/rest/server";
+    
+    public static final String GROUP_ID = "org.kie.example";
+    public static final String ARTIFACT_ID = "project3";
+    public static final String VERSION = "1.0.0-SNAPSHOT";
+    
+    public static final String CONTAINER_ID = GROUP_ID + ":" + ARTIFACT_ID + ":" + VERSION;
+    
+}
