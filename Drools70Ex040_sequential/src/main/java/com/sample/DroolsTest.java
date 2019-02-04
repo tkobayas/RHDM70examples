2056fc9986b9f404f56a8195fa3eaa7fd11b13ec
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2019-02-04 15:02:47 +0900

    sequential mode

---
+++ Drools70Ex040_sequential/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,38 @@
+package com.sample;
+
+import java.util.Arrays;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.internal.command.CommandFactory;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            
+            // If you set sequential mode to true, you should not use re-evaluation by update/modify.
+            // If you have update/modify in rules, the result may differ depending on rules ordering in a DRL file
+            System.setProperty("drools.sequential", "true");
+            
+            // load up the knowledge base
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            StatelessKieSession kSession = kContainer.newStatelessKieSession(); // can use only stateless
+
+            // go !
+            Person john = new Person("john", 20);
+
+            kSession.execute( CommandFactory.newInsertElements(Arrays.asList( john )) );
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
