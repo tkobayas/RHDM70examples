823f4aeb640860aa21500c25142d138b624f05da
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2019-02-13 17:36:59 +0900

    fix for 7.0.0

--- Drools70Ex024_kie-server-client/src/main/java/com/sample/Constants.java
+++ Drools70Ex024_kie-server-client/src/main/java/com/sample/Constants.java
@@ -9,8 +9,9 @@ public class Constants {
     
     public static final String GROUP_ID = "org.kie.example";
     public static final String ARTIFACT_ID = "project3";
-    public static final String VERSION = "1.0.0-SNAPSHOT";
-    
+//    public static final String VERSION = "1.0.0-SNAPSHOT";
+    public static final String VERSION = "5.0.0";
+
     public static final String CONTAINER_ID = GROUP_ID + ":" + ARTIFACT_ID + ":" + VERSION;
     
 }
