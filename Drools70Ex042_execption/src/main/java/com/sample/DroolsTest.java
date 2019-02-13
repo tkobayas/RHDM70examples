8e084a1bc14bad8c4c847d78841f0d7715660dfc
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2019-02-13 18:03:28 +0900

    exception handling

---
+++ Drools70Ex042_execption/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,71 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.ConsequenceException;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+            kSession.fireAllRules();
+
+            kSession.dispose();
+
+        } catch (Throwable t) {
+            System.out.println("t.getClass() = " + t.getClass());
+            
+            if (t instanceof ConsequenceException) {
+                ConsequenceException ce = (ConsequenceException)t;
+                System.out.println(ce.getMatch());
+                System.out.println(ce.getRule());
+                System.out.println(ce.getMessage());
+                ce.printFacts();
+            }
+
+            t.printStackTrace();
+        }
+    }
+
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+
+}
