6b0ae2dd6734780855d92637cb5699b462a89109
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2019-03-19 10:31:24 +0900

    not working yet

---
+++ Drools72Ex045_ksession_serialization/src/main/java/com/sample/DeserializeTest.java
@@ -0,0 +1,45 @@
+package com.sample;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.drools.core.common.DroolsObjectInputStream;
+import org.drools.core.common.InternalWorkingMemory;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DeserializeTest {
+
+    public static final void main(String[] args) {
+        // load up the knowledge base
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieBase kBase = kContainer.getKieBase();
+        KieSession kSession = null;
+
+        // Deserialize
+        try (FileInputStream in = new FileInputStream("./ksession.out");
+                DroolsObjectInputStream dois = new DroolsObjectInputStream(in);) {
+            
+            kSession = (KieSession) dois.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // go !
+        Person paul = new Person("Paul", 30);
+        kSession.insert(paul);
+        kSession.fireAllRules();
+
+        kSession.dispose();
+
+    }
+
+}
---
+++ Drools72Ex045_ksession_serialization/src/main/java/com/sample/SerializeTest.java
@@ -0,0 +1,41 @@
+package com.sample;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.drools.core.common.DroolsObjectOutputStream;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class SerializeTest {
+
+    public static final void main(String[] args) {
+        // load up the knowledge base
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession kSession = kContainer.newKieSession();
+
+        // go !
+        Person john = new Person("John", 10);
+        kSession.insert(john);
+        
+        kSession.fireAllRules();
+
+        // Serialize
+        try (FileOutputStream out = new FileOutputStream("./ksession.out");
+                DroolsObjectOutputStream doos = new DroolsObjectOutputStream(out)) {
+
+            doos.writeObject(kSession);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
